732b7ff99d21cf0618f6e239970d9497ca9791d3
saxermi <137685346+Saxermi@users.noreply.github.com>
2023-12-09 12:44:10 +0100

    added functionality to use nearby amneiites function as a python function

--- code/Prototypen/Prototyp_2/filters/amenities.py
+++ code/Prototypen/Prototyp_2/filters/amenities.py
@@ -21,6 +21,59 @@ def some_route():
     return jsonify({"message": "This is an example from amenities.py"})
 
 
+def fetch_nearby_amenities(latitude, longitude, distance_km, category, limit):
+    """
+    Core functionality to fetch nearby amenities.
+    """
+    try:
+        # Connect to MongoDB
+        collection = get_amenities_collection()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        logging.error("Error in fetch_nearby_amenities: %s", e, exc_info=True)
+        return []
+
+    # Define the center point (longitude, latitude) and radius in radians
+    center_point = [longitude, latitude]
+    radius_radians = distance_km / 6371  # Earth's radius in kilometers
+
+    query = {
+        "location": {"$geoWithin": {"$centerSphere": [center_point, radius_radians]}}
+    }
+
+    projection = {"_id": 0}  # Modify as needed
+    results = collection.find(query, projection).limit(limit)
+
+    # Filtering logic based on 'category'
+    if category in ["fbs", "ecv", "pcs", "ths"]:
+        categories = {
+            "fbs": ["bar", "cafe", "fast_food", "food_court", "restaurant", "pub"],
+            "ecv": [
+                "arts_centre",
+                "casino",
+                "cinema",
+                "events_venue",
+                "music_venue",
+                "nightclub",
+                "theatre",
+            ],
+            "pcs": ["library", "atm", "bank", "police", "post_box", "post_office"],
+            "ths": [
+                "bus_station",
+                "bicycle_parking",
+                "bicycle_repair_station",
+                "doctors",
+                "hospital",
+                "pharmacy",
+            ],
+        }
+        results = [doc for doc in results if doc.get("amenity") in categories[category]]
+    else:
+        results = list(results)
+
+    return results
+
+
 @bp.route("/get_amenitites_nearby", methods=["POST"])
 def get_nearby_amenities():
     latitude = request.form.get("latitude", type=float)
@@ -32,124 +85,26 @@ def get_nearby_amenities():
     Function to get nearby amenities based on a center point and a specified distance.
     Uses centerSphere calculations to determine the area of interest and performs a MongoDB query.
     """
-    try:
-        # Connect to MongoDB
-        collection = get_amenities_collection()
-    except Exception as e:
-        # Print any error encountered during the connection attempt
-        # print("Fehler beim Abrufen der Datenbankliste:", e)
-        print(f"An error occurred: {e}")
-        logging.error("Error in get_nearby_amenities: %s", e, exc_info=True)
-
-        # Define the center point (longitude, latitude) and radius in radians
-    center_point = [longitude, latitude]
-    radius_radians = distance_km / 6371  # Earth's radius in kilometers
-    query = {
-        "location": {"$geoWithin": {"$centerSphere": [center_point, radius_radians]}}
-    }
-    projection = {
-        "_id": 0
-        # "name": 1
-    }
-    # Execute the query in the database
-    # 'find()' returns a cursor that iterates through the found documents
-    results = collection.find(query, projection).limit(limit)
-    # Food and Beverage Services
-
-    # for document in results:
-    #   print(document)
-
-    food_beverage_services = [
-        "bar",
-        "cafe",
-        "fast_food",
-        "food_court",
-        "restaurant",
-        "pub",
-    ]
-
-    # Entertainment and Cultural Venues
-    entertainment_cultural = [
-        "arts_centre",
-        "casino",
-        "cinema",
-        "events_venue",
-        "music_venue",
-        "nightclub",
-        "theatre",
-    ]
-
-    # Public and Civic Services
-    public_civic_services = [
-        "library",
-        "atm",
-        "bank",
-        "police",
-        "post_box",
-        "post_office",
-    ]
-
-    # Transportation and Health Services
-    transportation_health = [
-        "bus_station",
-        "bicycle_parking",
-        "bicycle_repair_station",
-        "doctors",
-        "hospital",
-        "pharmacy",
-    ]
-    # return the category
-
-    # Initialize an empty list to store the filtered documents
-    new_docs = []
-
-    if category == "fbs":
-        new_docs = [
-            doc for doc in results if doc.get("amenity") in food_beverage_services
-        ]
-
-    elif category == "ecv":
-        new_docs = [
-            doc for doc in results if doc.get("amenity") in entertainment_cultural
-        ]
-
-    elif category == "pcs":
-        new_docs = [
-            doc for doc in results if doc.get("amenity") in public_civic_services
-        ]
-
-    elif category == "ths":
-        new_docs = [
-            doc for doc in results if doc.get("amenity") in transportation_health
-        ]
-
-    elif category is None:
-        # Handle the None case as needed
-        new_docs = list(results)
-    else:
-        new_docs = list(results)
-
-    # Print each document in new_docs
-    for doc in new_docs:
-        print(doc)
+    new_docs = fetch_nearby_amenities(latitude, longitude, distance_km, category, limit)
 
     # Return the JSON response
     return jsonify(new_docs)
 
 
-@bp.route("/get_amenitites_nearby_scatter")
-def get_amenities_scatterplot(
-    inputlatitude, inputlongitude, distance_km=0.5, limit=10000, category=None
-):
-    # Fetches nearby amenities based on provided latitude, longitude, distance, and category.
-    # The function 'get_nearby_amenities' is assumed to return a list of amenities
-    # within the specified distance from the input coordinates.
-    data = get_nearby_amenities(
-        inputlatitude, inputlongitude, distance_km, category=category
-    )
+@bp.route("/get_amenitites_nearby_scatter", methods=["POST"])
+def get_amenities_scatterplot():
+    # Extract parameters from request.form
+    latitude = request.form.get("latitude", type=float)
+    longitude = request.form.get("longitude", type=float)
+    distance_km = request.form.get("distance_km", default=0.5, type=float)
+    category = request.form.get("category")
+    limit = request.form.get("limit", default=10000, type=int)
+
+    # Call the helper function
+    data = fetch_nearby_amenities(latitude, longitude, distance_km, category, limit)
 
     # Your input coordinates as a tuple.
-    input_coordinates = (inputlatitude, inputlongitude)
+    input_coordinates = (latitude, longitude)
     # Initialize a dictonary to store distances from the input coordinates to each amenity.
     distances = {}
     dictid = 0
@@ -189,12 +144,20 @@ def get_amenities_scatterplot(
     return jsonify(distances)
 
 
-@bp.route("/get_amenitites_nearby_barrchart")
-def get_amenities_barchart(
-    latitude, longitude, distance_km=0.5, limit=10000, category=None, sortbycat=False
-):
+@bp.route("/get_amenitites_nearby_barrchart", methods=["POST"])
+def get_amenities_barchart():
+    # Extract parameters from request.form
+    latitude = request.form.get("latitude", type=float)
+    longitude = request.form.get("longitude", type=float)
+    distance_km = request.form.get("distance_km", default=0.5, type=float)
+    category = request.form.get("category")
+    limit = request.form.get("limit", default=10000, type=int)
+    sortbycat = request.form.get("sortbycat", default=False, type=bool)
+
+    # Call the helper function
+    data = fetch_nearby_amenities(latitude, longitude, distance_km, category, limit)
+
     # Get nearby amenities based on the provided latitude, longitude, and other parameters.
-    data = get_nearby_amenities(latitude, longitude, distance_km, category=category)
 
     # Initialize a dictionary to store counts of each amenity.
     amenity_counts = {}
